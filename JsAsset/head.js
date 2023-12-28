f982b6986ed49a9b2826f10a8458417aea28186c
Arnab <arnabk2001@gmail.com>
2023-12-28 18:43:21 +0530

    New Header made and imported

--- JsAsset/head.js
+++ JsAsset/head.js
@@ -3,12 +3,25 @@
 
   console.log(e);
 } */
-const ex = document.getElementById("myLink");
-if (ex) {
-  ex.addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log(event);
-  });
-} else {
-  console.log("there is an error");
+function NavButton(e) {
+  const itemColumn = document.getElementById("ItemColumn");
+  if (itemColumn.style.display === "none") {
+    itemColumn.style.display = "flex";
+  } else {
+    itemColumn.style.display = "none";
+  }
 }
+/* function Item(event) {
+  e.preventDefault();
+  e.stopPropagation();
+} */
+document.getElementById("ItemRow").addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    console.log("List item clicked!");
+    console.log(event); // Access event object
+  } else if (event.target.tagName === "A") {
+    console.log("Anchor clicked!");
+    console.log(event); // Access event object
+    event.stopPropagation(); // Optionally stop event propagation
+  }
+});
